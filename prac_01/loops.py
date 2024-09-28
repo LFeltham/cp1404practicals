0d21f904a3f5908a14148a1585028fdb6c2a049e
Leah Feltham <leah.feltham@my.jcu.edu.au>
2024-09-29 09:10:39 +1000

    Add prac_01 files

---
+++ prac_01/loops.py
@@ -0,0 +1,26 @@
+# A
+for i in range(0, 101, 10):
+    print(i, end=' ')
+
+print()
+
+
+# B
+for i in range(20, 0, -1):
+    print(i, end=' ')
+
+print()
+
+
+# C
+num_of_stars = int(input("Number of stars: "))
+for i in range(num_of_stars):
+    print('*', end=' ')
+
+print()
+
+# D
+for i in range(1, num_of_stars + 1):
+    print('*' * i)
+
+print()
