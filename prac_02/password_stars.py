4ddbd745eee3d404cc5a397d52d32c9c390e0a22
Leah Feltham <leah.feltham@my.jcu.edu.au>
2024-10-17 21:30:29 +1000

    Refactor password check program to use functions

---
+++ prac_02/password_stars.py
@@ -0,0 +1,26 @@
+MINIMUM_LENGTH = 4
+# def original():
+#     password = input(f"Enter password: ")
+#     while len(password) < MINIMUM_LENGTH:
+#         password = input(f"Enter password: ")
+#     print('*' * len(password))
+
+
+def main():
+    password = get_password(MINIMUM_LENGTH)
+    asterisks_for_password(password)
+
+
+def get_password(minimum_length):
+    password = input(f"Enter password: ")
+    while len(password) < minimum_length:
+        print(f"Password too short, must be at least {minimum_length} characters: ")
+        password = input(f"Enter password: ")
+    return password
+
+
+def asterisks_for_password(sequence):
+    print('*' * len(sequence))
+
+
+main()
